6e5efc78c8c12dc54676171cbd1a22b56d5a29f9
Michele Balbinot <michele.balbinot@marconirovereto.it>
2018-03-23 00:02:30 +0100

    aggiunta recita

---
+++ src/types/Recita.ts
@@ -0,0 +1,11 @@
+export class Recita{
+  CodAttore : number
+  CodFlim : number
+
+  constructor(
+     codAttore:number, codFilm: number
+  ){
+    this.CodAttore = codAttore;
+    this.CodFlim = codFilm;
+  }
+}
